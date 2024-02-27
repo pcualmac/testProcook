f94f216353cd378b3cd4038b5a12c96137770b41
Calogero Pumpignano <calogero.pumpignano@hoistgroup.com>
2024-02-27 16:50:15 +0000

    use middleware auth

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/comparison', [App\Http\Controllers\Comparison::class, 'index'])->name('comparison');
+Route::get('/comparison', [App\Http\Controllers\Comparison::class, 'index'])->middleware('auth')->name('comparison');
 // //{product_1}/{product_2}/{product_3?}/{product_4?}
