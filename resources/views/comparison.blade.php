7be38319e93955a8cb988dd4274fe4fc0824a11f
Calogero Pumpignano <calogero.pumpignano@hoistgroup.com>
2024-02-27 16:58:46 +0000

    work on comparison.blade.php

--- resources/views/comparison.blade.php
+++ resources/views/comparison.blade.php
@@ -3,6 +3,14 @@
 @section('content')
 <div class="container">
     <div class="card-columns">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Colour</li>
+                <li class="list-group-item">Warranty</li>
+            </ul>
+        </div>
         @foreach ($data as $card)
         <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="{{ asset('storage/image/'.$card->image.'.jpg')}}" alt="Card image cap">
